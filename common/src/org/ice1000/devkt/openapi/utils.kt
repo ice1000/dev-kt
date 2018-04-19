59517cc0971d8600cb11de57dacf92da1e5a94a0
ice1000 <ice1000kotlin@foxmail.com>
2018-04-20 01:57:52 +0800

    [ utils ] Add common utils

--- common/src/org/ice1000/devkt/openapi/utils.kt
+++ common/src/org/ice1000/devkt/openapi/utils.kt
@@ -1,6 +1,38 @@
+@file:JvmName("PsiUtils")
+
 package org.ice1000.devkt.openapi
 
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.com.intellij.psi.tree.IElementType
 
 val PsiElement.nodeType: IElementType get() = node.elementType
+
+inline fun <reified Psi : PsiElement> PsiElement.nextSiblingIgnoring(vararg types: IElementType): Psi? {
+	var next: PsiElement? = nextSibling
+	while (true) {
+		val localNext = next ?: return null
+		next = localNext.nextSibling
+		return if (types.any { localNext.node.elementType == it }) continue
+		else localNext as? Psi
+	}
+}
+
+inline fun <reified Psi : PsiElement> PsiElement.prevSiblingIgnoring(vararg types: IElementType): Psi? {
+	var next: PsiElement? = prevSibling
+	while (true) {
+		val localNext = next ?: return null
+		next = localNext.prevSibling
+		return if (types.any { localNext.node.elementType == it }) continue
+		else localNext as? Psi
+	}
+}
+
+fun PsiElement.childrenBefore(type: IElementType): List<PsiElement> {
+	val ret = ArrayList<PsiElement>()
+	var next: PsiElement? = firstChild
+	while (true) {
+		next = next?.nextSibling ?: return ret
+		if (next.node.elementType == type) return ret
+		ret += next
+	}
+}
