5d80bd4fd2a384fefaceefd89ca2fde850b4dbe9
LimbolRain <limbolrain@gmail.com>
2018-04-24 06:11:22 +0800

    [ find ] matches count added

--- swing/src/org/ice1000/devkt/ui/swing/dialogs/find-replace.kt
+++ swing/src/org/ice1000/devkt/ui/swing/dialogs/find-replace.kt
@@ -13,59 +13,64 @@ import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
 abstract class FindUI : JDialog() {
-	protected val mainPanel = JPanel()
-	protected val isMatchCase = JCheckBox()
-	protected val moveUp = JButton()
-	protected val moveDown = JButton()
-	protected val isRegex = JCheckBox()
-	protected val findInput = JTextField()
-	protected val replaceInput = JTextField()
-	protected val replace = JButton()
-	protected val replaceAll = JButton()
-	protected val separator = JSeparator()
+	protected val mainPanel: JPanel = JPanel()
+	protected val isMatchCase: JCheckBox = JCheckBox()
+	protected val moveUp: JButton = JButton()
+	protected val moveDown: JButton = JButton()
+	protected val isRegex: JCheckBox = JCheckBox()
+	protected val findInput: JTextField = JTextField()
+	protected val replaceInput: JTextField = JTextField()
+	protected val replace: JButton = JButton()
+	protected val replaceAll: JButton = JButton()
+	protected val separator: JSeparator = JSeparator()
+	protected val findCount: JLabel = JLabel()
 
 	init {
 		mainPanel.layout = GridLayoutManager(5, 1, Insets(0, 0, 0, 0), -1, -1)
-		val panel0 = JPanel()
-		panel0.layout = GridLayoutManager(1, 2, Insets(0, 0, 0, 0), -1, -1)
-		mainPanel.add(panel0, GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
+		val panel1 = JPanel()
+		panel1.layout = GridLayoutManager(1, 2, Insets(0, 0, 0, 0), -1, -1)
+		mainPanel.add(panel1, GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
 		isMatchCase.text = "Match Case"
 		isMatchCase.setMnemonic('C')
 		isMatchCase.displayedMnemonicIndex = 6
-		panel0.add(isMatchCase, GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		panel1.add(isMatchCase, GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
 		isRegex.text = "Regex"
 		isRegex.setMnemonic('G')
 		isRegex.displayedMnemonicIndex = 2
-		panel0.add(isRegex, GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		panel1.add(isRegex, GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
 		mainPanel.add(findInput, GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, Dimension(150, -1), null, 0, false))
-		val panel1 = JPanel()
-		panel1.layout = GridLayoutManager(1, 3, Insets(0, 0, 0, 0), -1, -1)
-		mainPanel.add(panel1, GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
-		moveUp.icon = DevKtIcons.MOVE_UP
+		val panel2 = JPanel()
+		panel2.layout = GridLayoutManager(1, 5, Insets(0, 0, 0, 0), -1, -1)
+		mainPanel.add(panel2, GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
+		moveUp.icon = ImageIcon(javaClass.getResource("/icon/moveUp.png"))
 		moveUp.text = ""
-		panel1.add(moveUp, GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
-		moveDown.icon = DevKtIcons.MOVE_DOWN
+		panel2.add(moveUp, GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		moveDown.icon = ImageIcon(javaClass.getResource("/icon/moveDown.png"))
 		moveDown.text = ""
-		panel1.add(moveDown, GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		panel2.add(moveDown, GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
 		val spacer1 = Spacer()
-		panel1.add(spacer1, GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false))
+		panel2.add(spacer1, GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false))
+		findCount.text = "0/0"
+		panel2.add(findCount, GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		val spacer2 = Spacer()
+		panel2.add(spacer2, GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false))
 		separator.isVisible = false
 		mainPanel.add(separator, GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false))
-		val panel2 = JPanel()
-		panel2.layout = GridLayoutManager(2, 2, Insets(0, 0, 0, 0), -1, -1)
-		mainPanel.add(panel2, GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
+		val panel3 = JPanel()
+		panel3.layout = GridLayoutManager(2, 2, Insets(0, 0, 0, 0), -1, -1)
+		mainPanel.add(panel3, GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false))
 		replaceInput.isVisible = false
-		panel2.add(replaceInput, GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, Dimension(150, -1), null, 0, false))
+		panel3.add(replaceInput, GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, Dimension(150, -1), null, 0, false))
 		replace.text = "Replace"
 		replace.setMnemonic('R')
 		replace.displayedMnemonicIndex = 0
 		replace.isVisible = false
-		panel2.add(replace, GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		panel3.add(replace, GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
 		replaceAll.text = "Replace all"
 		replaceAll.setMnemonic('A')
 		replaceAll.displayedMnemonicIndex = 8
 		replaceAll.isVisible = false
-		panel2.add(replaceAll, GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
+		panel3.add(replaceAll, GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK or GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false))
 	}
 }
 
@@ -94,6 +99,10 @@ open class FindDialogImpl(
 			override fun insertUpdate(e: DocumentEvent?) = removeUpdate(e)
 			override fun removeUpdate(e: DocumentEvent?) = search(bundle)
 		})
+	}
+
+	override fun update() {
+		findCount.text = "${currentIndex.takeIf { searchResult.size != 0 }?.plus(1) ?: 0}/${searchResult.size}"
 	}
 
 	final override fun setLocationRelativeTo(c: Component?) = super.setLocationRelativeTo(c)
