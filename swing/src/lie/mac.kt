558335d8eba65ec18111e151888330f43c7db982
nanangarsyad <nanangarsyad@mailtarget.co>
2020-06-10 20:40:56 +0700

    fix failed to build for mac

--- swing/src/lie/mac.kt
+++ swing/src/lie/mac.kt
@@ -1,35 +1,37 @@
 package org.ice1000.devkt.lie
 
-import com.apple.eawt.*
-import com.apple.eawt.Application.getApplication
 import org.ice1000.devkt.config.GlobalSettings
 import org.ice1000.devkt.ui.swing.DevKtFrame
 import org.jetbrains.kotlin.com.intellij.openapi.util.SystemInfo
+import java.awt.Desktop.getDesktop
+import java.awt.Taskbar.getTaskbar
+import java.awt.desktop.*
 import java.awt.event.KeyEvent
 
 val ctrlOrMeta = if (SystemInfo.isMac) KeyEvent.META_DOWN_MASK else KeyEvent.CTRL_DOWN_MASK
 
-object MacSpecific : AboutHandler, PreferencesHandler, QuitHandler {
+object MacSpecific : AboutHandler, PreferencesHandler, QuitHandler  {
 	init {
 		// if System is Mac, make sure set this property before setLookAndFeel
 		System.getProperties()["apple.laf.useScreenMenuBar"] = "true"
 	}
 
 	fun init() {
-		getApplication().let { app ->
+		getDesktop().let { app ->
 			app.setPreferencesHandler(this)
 			app.setQuitHandler(this)
 			app.setAboutHandler(this)
-			app.dockIconImage = GlobalSettings.windowIcon.second
 		}
+		getTaskbar().iconImage = GlobalSettings.windowIcon.second
 	}
 
 	private val ui
 		get() = DevKtFrame.instance.ui
 
-	override fun handlePreferences(event: AppEvent.PreferencesEvent) = ui.settings()
-	// TODO About
-	override fun handleAbout(event: AppEvent.AboutEvent) = Unit
+	override fun handlePreferences(e: PreferencesEvent?) = ui.settings()
 
-	override fun handleQuitRequestWith(event: AppEvent.QuitEvent, quitResponse: QuitResponse) = ui.exit()
+	// TODO About
+	override fun handleAbout(e: AboutEvent?) = Unit
+
+	override fun handleQuitRequestWith(e: QuitEvent?, response: QuitResponse?) = ui.exit()
 }
